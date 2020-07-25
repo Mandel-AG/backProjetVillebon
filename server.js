271e8602c48dfdfbb0c49b80075f8120bc771514
Mandel <a.mandel06@gmail.com>
2020-07-26 01:09:25 +0200

    add origin true to cors

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(express.static(path.join(__dirname,'teams')));
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json())
-app.use(cors());
+app.use(cors({ origin: true }));
 app.use(routing)
 
 
