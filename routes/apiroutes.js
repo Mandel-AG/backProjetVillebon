567dacd2126541d8a45d466dd507a8ca929196da
Mandel <a.mandel06@gmail.com>
2020-07-26 07:07:54 +0200

    commit -1

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -31,6 +31,89 @@ conn.once('open', function () {
 app.post('/new',createAdmin)
 
 
+app.get('/medias',async(req,res, next) => {
+    try{
+        const medias = await Media.find({}).exec()
+        res.json(medias);
+    }catch(e){
+        next(e);
+    }
+ })
+
+
+
+app.get('/events',async(req,res, next) => {
+   try{
+       const events = await Event.find({}).exec()
+       res.json(events);
+   }catch(e){
+       next(e);
+   }
+})
+
+
+app.get('/scores',async (req,res, next) => {
+   try{
+       const scores = await Score.find({}).exec()
+       res.json(scores);
+   }
+   catch(e){
+       next(e);
+   }
+})
+
+
+app.get('/clubs',async(req,res, next) => {
+   try{
+       const clubs = await Club.find({}).exec()
+       res.json(clubs);
+   }catch(e){
+       next(e);
+   }
+})
+
+
+app.get('/teams',async(req,res, next) => {
+    try{
+        const teams = await Team.find({}).exec()
+        res.json(teams);
+    }catch(e){
+        next(e);
+    }
+ })
+
+
+
+app.get('/gyms',async(req,res, next) => {
+    try{
+        const gyms = await Gym.find({}).exec()
+        res.json(gyms);
+    }catch(e){
+        next(e);
+    }
+})
+
+
+app.get('/products',async(req,res, next) => {
+    try{
+        const products = await Product.find({}).exec()
+        res.json(products);
+    }catch(e){
+        next(e);
+    }
+})
+
+
+app.get('/members',async(req,res, next) => {
+    try{
+        const members = await Member.find({}).exec()
+        res.json(members);
+    }catch(e){
+        next(e);
+    }
+})
+
+
 
 
 // app.get('/pictures/files/:fileName',async(req,res, next) => {
@@ -203,86 +286,6 @@ app.get('/teams/files/:fileid',(req,res, next) => {
 
 
 
-// app.get('/medias',async(req,res, next) => {
-//     try{
-//         const medias = await Media.find({}).exec()
-//         res.json(medias);
-//     }catch(e){
-//         next(e);
-//     }
-//  })
 
-
-
-// app.get('/events',async(req,res, next) => {
-//    try{
-//        const events = await Event.find({}).exec()
-//        res.json(events);
-//    }catch(e){
-//        next(e);
-//    }
-// })
-
-
-// app.get('/scores',async (req,res, next) => {
-//    try{
-//        const scores = await Score.find({}).exec()
-//        res.json(scores);
-//    }
-//    catch(e){
-//        next(e);
-//    }
-// })
-
-
-// app.get('/clubs',async(req,res, next) => {
-//    try{
-//        const clubs = await Club.find({}).exec()
-//        res.json(clubs);
-//    }catch(e){
-//        next(e);
-//    }
-// })
-
-
-// app.get('/teams',async(req,res, next) => {
-//     try{
-//         const teams = await Team.find({}).exec()
-//         res.json(teams);
-//     }catch(e){
-//         next(e);
-//     }
-//  })
-
-
-
-// app.get('/gyms',async(req,res, next) => {
-//     try{
-//         const gyms = await Gym.find({}).exec()
-//         res.json(gyms);
-//     }catch(e){
-//         next(e);
-//     }
-// })
-
-
-// app.get('/products',async(req,res, next) => {
-//     try{
-//         const products = await Product.find({}).exec()
-//         res.json(products);
-//     }catch(e){
-//         next(e);
-//     }
-// })
-
-
-// app.get('/members',async(req,res, next) => {
-//     try{
-//         const members = await Member.find({}).exec()
-//         res.json(members);
-//     }catch(e){
-//         next(e);
-//     }
-// })
 
 module.exports = app;
